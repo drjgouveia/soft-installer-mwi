9ea21d9cfafb6cd30a9e38d4e5fd65a6f2f8f848
drjgouveia <56302426+drjgouveia@users.noreply.github.com>
2021-06-09 14:43:16 +0200

    First

---
+++ main.py
@@ -0,0 +1,108 @@
+import subprocess
+import winwifi
+from time import sleep
+from tkinter import *
+import os
+
+software = [("Anydesk", "AnyDeskSoftwareGmbH.AnyDeskMSI"),
+            ("Firefox", "Mozilla.FirefoxESR"),
+            ("Google Chrome", "Google.Chrome"),
+            ("7-Zip", "7zip.7zip"),
+            ("K-Lite Codecs", "CodecGuide.K-LiteCodecPackStandard"),
+            ("VLC", "VideoLAN.VLC"),
+            ("Zoom", "Zoom.Zoom"),
+            ("Adobe Reader DC", "Adobe.AdobeAcrobatReaderDC"),
+            ("LibreOffice", "LibreOffice.LibreOffice"),
+            ("OpenJDK 14", "AdoptOpenJDK.OpenJDK.14"),
+            ("OpenJDK 15", "AdoptOpenJDK.OpenJDK.15"),
+            ("OpenJDK 16", "AdoptOpenJDK.OpenJDK.16")]
+
+soft = []
+
+
+class MyFirstGUI:
+    def __init__(self, master):
+        self.master = master
+        self.master.iconbitmap('icon.ico')
+        self.master.title("Software MWI installer")
+        self.top = Frame(self.master)
+        self.mid = Frame(self.master)
+        self.bottom = Frame(self.master)
+        self.top.pack(side=TOP)
+        self.mid.pack(side=TOP)
+        self.bottom.pack(side=BOTTOM, fill=BOTH, expand=True)
+
+        self.label = Label(master, text="Elija el software")
+        self.label.pack(in_=self.top, side=TOP, fill=X, ipady=10)
+        self.label.config(font=("Arial", 16), anchor=CENTER)
+
+#        i = 1
+#        u = 0
+        k = 0
+#        self.label.grid(row=0, column=0, columnspan=2)
+
+        for name, package in software:
+            soft.append(Variable(value=(0, package)))
+            self.option_button = Checkbutton(master, width=75,text=name, onvalue=(1, package), offvalue=(0, package),
+                                             variable=soft[k])
+#            self.option_button.grid(row=i, column=u)
+            self.option_button.pack(in_=self.mid, fill=X)
+            k += 1
+
+        self.install_button = Button(master, text="Install", command=self.install)
+        self.install_button.config(anchor=CENTER)
+        self.install_button.pack(in_=self.bottom, side=LEFT, ipadx=50, padx=50)
+
+        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button.config(anchor=CENTER)
+        self.close_button.pack(in_=self.bottom, side=RIGHT, ipadx=50, padx=50)
+
+
+    def install(self):
+        total = 0
+        for s in soft:
+            active = int(s.get()[0])
+            if active == 1:
+                total += 1
+
+        ini = "winget install --id="
+        line = ini
+        x = 0
+
+        for s in soft:
+            active = int(s.get()[0])
+            if active == 1:
+                name = str(s.get()).replace("1 ", "")
+                line = line + name + " -e --silent"
+                os.system(line)
+                line = ini
+
+if __name__ == '__main__':
+    try:
+        print("Conectando a la Wifi...")
+        ssid = "MagicWorld - SAT"
+        password = ""
+        winwifi.WinWiFi.connect(ssid, password)
+        print("Conectado.\n")
+        sleep(5)
+
+    except Exception as e:
+        print("Hubo un error con la conexion a la Wifi.")
+        if input("Te gustaria continuar? (Y/n) ") in ("N", "n"):
+            exit()
+
+    try:
+        print("Instalando WinGet...")
+        cmd = "Add-AppxPackage \".\\Microsoft.DesktopAppInstaller.appxbundle\""
+        completed = subprocess.run(["powershell", "-Command", cmd], capture_output=True)
+        print("Instalado.\n")
+    except Exception as e:
+        pass
+
+    print("Empienzando la GUI.")
+    root = Tk()
+    x = 50 + 30 * len(software)
+    root.geometry("400x"+str(x))
+    my_gui = MyFirstGUI(root)
+    root.mainloop()
+    print("Bye!")
---
+++ ven/Scripts/wifi-script.py
@@ -0,0 +1,33 @@
+#!C:\Users\User\PycharmProjects\mwi\ven\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'winwifi==1.0.3','console_scripts','wifi'
+import re
+import sys
+
+# for compatibility with easy_install; see #2198
+__requires__ = 'winwifi==1.0.3'
+
+try:
+    from importlib.metadata import distribution
+except ImportError:
+    try:
+        from importlib_metadata import distribution
+    except ImportError:
+        from pkg_resources import load_entry_point
+
+
+def importlib_load_entry_point(spec, group, name):
+    dist_name, _, _ = spec.partition('==')
+    matches = (
+        entry_point
+        for entry_point in distribution(dist_name).entry_points
+        if entry_point.group == group and entry_point.name == name
+    )
+    return next(matches).load()
+
+
+globals().setdefault('load_entry_point', importlib_load_entry_point)
+
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(load_entry_point('winwifi==1.0.3', 'console_scripts', 'wifi')())
